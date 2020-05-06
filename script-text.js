7f29999d945142bd85d24aab4b455f60a9ca4d7e
Nathalie DB <46918022+NathDB@users.noreply.github.com>
2020-05-06 17:15:59 +0200

    add content

---
+++ script-text.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typed', {
+    strings: ["Bienvenue", "Etudiante en informatique", "En recherche d'alternance", "DevOps", "Sociable", "Curieuse"],
+    typeSpeed: 80,
+    backSpeed: 70,
+    loop: true,
+    shuffle: true,
+    backDelay: 3000,
+    contentType: 'html'
+  });
